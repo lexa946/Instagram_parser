ab16da6d8f27a9df366d559562383c06aa20ff93
KUDAHTER <pozhara98@mail.ru>
2019-06-23 22:50:30 +0300

    Complite

--- main.py
+++ main.py
@@ -271,7 +271,8 @@ def main():
 
         bio = api.filter_data_for_day(7,file_in='filter_words', file_out='filter_all_for_7_days')
 
-
+        # dawd awd aw
+        # awdawdawd
 
     else:
         print("Can't login!")
